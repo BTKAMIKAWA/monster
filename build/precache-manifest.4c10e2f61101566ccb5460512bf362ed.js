eca496e18d190dd06d85373f73adabf85202dbf1
Brian Kamikawa <47756243+BTKAMIKAWA@users.noreply.github.com>
2019-11-04 15:03:45 -0800

    Add files via upload

---
+++ build/precache-manifest.4c10e2f61101566ccb5460512bf362ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae63ba604f5d06b4c33cf9bfc6370e6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4173bb731fb1f09f6414",
+    "url": "/monsters-rolodex/static/css/main.ada414ba.chunk.css"
+  },
+  {
+    "revision": "7b7fe9caed2c3ba0e555",
+    "url": "/monsters-rolodex/static/js/2.64425f82.chunk.js"
+  },
+  {
+    "revision": "4173bb731fb1f09f6414",
+    "url": "/monsters-rolodex/static/js/main.07f5d3d9.chunk.js"
+  },
+  {
+    "revision": "30eb7a04447b03b96b91",
+    "url": "/monsters-rolodex/static/js/runtime-main.9eb198d2.js"
+  }
+]);
